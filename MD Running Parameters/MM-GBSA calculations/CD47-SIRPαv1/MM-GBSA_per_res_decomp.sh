baa8b8de6101b6a74a20fcf24c65d74722985fdc
Ks1997cris <122732782+Ks1997cris@users.noreply.github.com>
2023-01-26 22:18:22 +0800

    Add files via upload

---
+++ MD Running Parameters/MM-GBSA calculations/CD47-SIRPαv1/MM-GBSA_per_res_decomp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# nohup bash mmgbsa_per_res_decomp.sh > mmgbsa_per_res_decomp.log 2>&1 &
+
+# Intel
+source /opt/intel/bin/compilervars.sh intel64
+export MKL_HOME=/opt/intel/mkl
+
+# Amber
+export AMBERHOME=/opt/amber20
+source $AMBERHOME/amber.sh
+
+
+########################
+# MMGBSA_DECOMP
+########################
+cat > mmgbsa.in << EOF
+Per-residue GB decomposition
+&general
+	startframe=1, endframe=50000, interval=5, 
+	verbose=1, keep_files=0, 
+	debug_printlevel=2, 
+/
+&gb
+	igb=2, saltcon=0, 
+/
+&decomp
+	idecomp=1, print_res="1-119; 120-237", csv_format=0, 
+	dec_verbose=1, 
+/
+
+EOF
+
+mpirun -np 4 MMPBSA.py.MPI -O -i mmgbsa.in -o FINAL_RESULTS_MMGBSA.dat -do FINAL_DECOMP_MMGBSA.dat -sp sys_sol.prmtop -cp dry.prmtop -rp sirpa.prmtop -lp CD47.prmtop -y ../../md/500ns/pr*.nc > mmgbsa_process.log
---
+++ MD Running Parameters/MM-GBSA calculations/CD47-SIRPαv1/run_leap_CD47.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cat > leap.in << EOF
+source leaprc.protein.ff14SB
+
+CD47 = loadpdb 4cmm_complete_cd47.pdb
+bond CD47.23.SG CD47.96.SG
+
+set default PBRadii mbondi2
+
+saveamberparm CD47 CD47.prmtop CD47.inpcrd
+savepdb CD47 CD47_leap.pdb
+
+quit
+EOF
+
+tleap -s -f leap.in > leap_out.log
